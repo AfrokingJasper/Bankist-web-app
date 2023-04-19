3995f181bd6a8b83962cd0e7259c5d0e75258388
AfrokingJasper <fortuneoliseyenum12@gmail.com>
2023-04-19 03:20:17 -0700

    login/signup form

--- js/script.js
+++ js/script.js
@@ -1,36 +1,48 @@
 const viewPassword = document.querySelectorAll(".view-password");
 const hidePassword = document.querySelectorAll(".hide-password");
-console.log(hidePassword);
+const passwordInput = document.querySelectorAll(".password");
+const forms = document.querySelectorAll(".forms");
+const changeForm = document.querySelectorAll(".change-form");
+console.log(changeForm);
+
+changeForm.forEach((change) =>
+  change.addEventListener("click", function (e) {
+    e.preventDefault();
+    forms.forEach((form) => form.classList.toggle("fade-in"));
+  })
+);
 
 viewPassword.forEach((view) => {
   view.addEventListener("click", function () {
     view.classList.toggle("hidden");
     hidePassword.forEach((hide) => {
       hide.classList.toggle("hidden");
+      passwordInput.forEach((inp) => inp.setAttribute("type", "password"));
     });
   });
 });
 
-// hidePassword.forEach((hide) => {
-//   hide.addEventListener("click", function (v, h) {
-//     hide.classList.toggle("hidden");
-//     viewPassword.forEach((view) => {
-//       view.classList.toggle("hidden");
-//     });
-//   });
-// });
-
-const toggleHidden = function (view) {
-  view.classList.toggle("hidden");
-  // hide.forEach((h) => h.classList.toggle("hidden"));
-};
-// const toggleHidden = function (view, hide) {
-//   view.forEach((v) => v.classList.toggle("hidden"));
-//   hide.forEach((h) => h.classList.toggle("hidden"));
-// };
 hidePassword.forEach((hide) => {
-  hide.addEventListener("click", toggleHidden);
+  hide.addEventListener("click", function (v, h) {
+    hide.classList.toggle("hidden");
+    viewPassword.forEach((view) => {
+      view.classList.toggle("hidden");
+      passwordInput.forEach((inp) => inp.setAttribute("type", "text"));
+    });
+  });
 });
+
+// const toggleHidden = function (view) {
+//   view.classList.toggle("hidden");
+//   // hide.forEach((h) => h.classList.toggle("hidden"));
+// };
+// // const toggleHidden = function (view, hide) {
+// //   view.forEach((v) => v.classList.toggle("hidden"));
+// //   hide.forEach((h) => h.classList.toggle("hidden"));
+// // };
+// hidePassword.forEach((hide) => {
+//   hide.addEventListener("click", toggleHidden(hide));
+// });
 
 // hide.classList.toggle("hidden");
 // viewPassword.forEach((view) => {
